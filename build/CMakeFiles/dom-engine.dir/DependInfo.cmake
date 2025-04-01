e38017b5fd57aa8037e3fbbdf0805bc34388a25e
“Laddha <ayushladdha123@gmail.com>
2025-04-02 00:35:43 +0530

    running state

---
+++ build/CMakeFiles/dom-engine.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/interpreter.cpp" "CMakeFiles/dom-engine.dir/src/interpreter.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/interpreter.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/main.cpp" "CMakeFiles/dom-engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/main.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/node.cpp" "CMakeFiles/dom-engine.dir/src/node.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/node.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/parser.cpp" "CMakeFiles/dom-engine.dir/src/parser.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/parser.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/script.cpp" "CMakeFiles/dom-engine.dir/src/script.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/script.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/shell.cpp" "CMakeFiles/dom-engine.dir/src/shell.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/shell.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/tree.cpp" "CMakeFiles/dom-engine.dir/src/tree.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/tree.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/src/util.cpp" "CMakeFiles/dom-engine.dir/src/util.cpp.obj" "gcc" "CMakeFiles/dom-engine.dir/src/util.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/dom-engine.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dom-engine.dir/src/interpreter.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/interpreter.cpp.obj.d"
+  "CMakeFiles/dom-engine.dir/src/main.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/main.cpp.obj.d"
+  "CMakeFiles/dom-engine.dir/src/node.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/node.cpp.obj.d"
+  "CMakeFiles/dom-engine.dir/src/parser.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/parser.cpp.obj.d"
+  "CMakeFiles/dom-engine.dir/src/script.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/script.cpp.obj.d"
+  "CMakeFiles/dom-engine.dir/src/shell.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/shell.cpp.obj.d"
+  "CMakeFiles/dom-engine.dir/src/tree.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/tree.cpp.obj.d"
+  "CMakeFiles/dom-engine.dir/src/util.cpp.obj"
+  "CMakeFiles/dom-engine.dir/src/util.cpp.obj.d"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/bin/dom-engine.exe"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/bin/dom-engine.exe.manifest"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/bin/dom-engine.pdb"
+  "D:/VIT/SEM 6/Compiler Design/DOMEngine/bin/libdom-engine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dom-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
